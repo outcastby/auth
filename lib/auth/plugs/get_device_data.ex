901590115c4b123c3f106d9cdb3328e78a9310f8
Mikhail Kruglik <miheykrug@mail.ru>
2020-05-07 20:45:32 +0300

    Fix getting ip in GetDeviceData

--- lib/auth/plugs/get_device_data.ex
+++ lib/auth/plugs/get_device_data.ex
@@ -11,14 +11,23 @@ defmodule Auth.Plugs.GetDeviceData do
   end
 
   def call(conn, _opts),
-    do: put_private(conn, :absinthe, %{context: get_context(conn) ||| %{device_data: get_devaice_data(conn)}})
+    do: put_private(conn, :absinthe, %{context: get_context(conn) ||| %{device_data: get_device_data(conn)}})
+
+  def get_header(conn, header), do: conn |> get_req_header(header) |> List.first()
 
   defp get_context(conn), do: conn.private[:absinthe][:context] || %{}
 
-  defp get_devaice_data(conn),
+  defp get_device_data(conn),
     do: %{
       browser: Browser.name(conn),
       platform: Browser.full_platform_name(conn),
-      ip: conn.remote_ip
+      ip: client_ip(conn)
     }
+
+  defp client_ip(conn) do
+    case __MODULE__.get_header(conn, "x-forwarded-for") do
+      nil -> nil
+      ips -> ips |> String.split(",") |> List.first()
+    end
+  end
 end
